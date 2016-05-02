ec1ac670e98a922f65941ac3fd50f4359f7b0404
shado <shado@DESKTOP-5501BNK>
2016-05-02 18:13:51 +0300

    dsfgdsfg

---
+++ Assets/scri.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+[SelectionBase]
+public class scri : MonoBehaviour {
+
+
+    [ContextMenuItem("Reset", "ResetBiography")]
+    [Tooltip("Health value between 0 and 100.")]
+    public string playerBiography = "";
+    void ResetBiography() {
+        playerBiography = "";
+    }
+
+
+
+void Start () {
+
+
+      
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
